96ab2206c39d4b318f7b07b2a1229392650f6ea1
Sixten Peterson <sixten.peterson@winterzone.se>
2023-05-08 08:45:23 +0200

    Added album to track mock data

--- src/mock/results.js
+++ src/mock/results.js
@@ -1,26 +1,31 @@
 const results = [
     {
         song: "DNA",
+        album: "Project Of A Person",
         artist: "Lund",
         id: 0
     },
     {
         song: "JUMPSHOT",
+        album: "",
         artist: "Kuma The Third",
         id: 1
     },
     {
         song: "Paris",
+        album: "Now the Moon's Rising",
         artist: "$uicideboy$",
         id: 2
     },
     {
         song: "No One",
+        album: "",
         artist: "Kill Red",
         id: 3
     },
     {
         song: "MAMA",
+        album: "HOPE",
         artist: "NF",
         id: 4
     }
